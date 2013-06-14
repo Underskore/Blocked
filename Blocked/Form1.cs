cbc0d567c737487e2df704c8ab1722461b84c7dd
Jasper Catthoor <jasper.catthoor@olvp.be>
2013-06-14 16:11:07 +0200

    Version 1

---
+++ Blocked/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Blocked
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IO.get get = new IO.get();
+            string[] objects = get.objects("test.txt");
+            foreach (string sr in objects)
+                textBox1.Text += sr + "\r\n";
+        }
+    }
+}
+
+
+/*
+
+
+
+
+
+
+
+
+[objects]
+  
+ 
+
+
+[/objects]
+
+
+
+*/
---
+++ Blocked/IO.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+
+namespace Blocked
+{
+    class IO
+    {
+
+        public class get
+        {
+
+            public string[] objects(string filename)
+            {
+                string rawtext = getRawText(filename);
+                int amountofobjects = getAmountOfObjects(rawtext);
+                string[] rawobjects = getRawObjects(rawtext, amountofobjects);
+                return rawobjects;
+            }
+
+            string getRawText(string filename)
+            {
+                StreamReader sr = new StreamReader(filename);
+                return sr.ReadToEnd();
+            }
+
+            int getAmountOfObjects(string rawtext)
+            {
+                StringReader sr = new StringReader(rawtext);
+                string line = sr.ReadLine(); int i = 0;
+                while (line != "objects[") line = sr.ReadLine();
+                while (line != "]") { line = sr.ReadLine(); if (line != "]") i++; }
+                return i;
+            }
+
+            string[] getRawObjects(string rawtext, int amountofobjects)
+            {
+                StringReader sr = new StringReader(rawtext);
+                string line = sr.ReadLine(); int i = 0; string[] objects = new string[amountofobjects];
+                while (line != "objects[") line = sr.ReadLine();
+                while (line != "]") { line = sr.ReadLine(); if (line != "]") { objects[i] = line; i++; } }
+                return objects;
+            }
+            
+        }
+
+    }
+}
